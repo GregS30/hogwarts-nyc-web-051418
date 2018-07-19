68511070e9365f2cd91c891d0ad7ec2aaaf8fbaa
GregS30 <gjschmitt@live.com>
2018-07-19 13:13:48 -0400

    fleshing out all components

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,47 @@ import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
 import HogDisplay from './HogDisplay'
+import HogShow from './HogShow'
+import HogSearch from './HogSearch'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      page: 'index',
+    };
+  }
+
+  renderPage = () => {
+		switch(this.state.page) {
+      case 'index':
+        return this.renderDisplay()
+      case 'show':
+        return this.renderShow()
+      default:
+  			return null
+		}
+	}
+
+  renderDisplay = () => {
+    return null;
+  }
+
+  renderShow = () => {
+    return null;
+  }
+
+  renderSearch = () => {
+    return null;
+  }
+
   render() {
     return (
       <div className="App">
         < Nav />
-        < HogDisplay />
+      {this.renderSearch()}
+      {this.renderPage()}
       </div>
     )
   }
--- src/components/HogDisplay.js
+++ src/components/HogDisplay.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react'
 import hogs from '../porkers_data.js'
 import UUID from 'uuid'
+import HogShow from './HogShow'
 
 class HogDisplay extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      pigId: null
     }
   }
 
   getHogs = () => {
       return (
-        hogs.map((hog) => {
+        hogs.map((hog, index) => {
         const hog_url = './hog-imgs/' + hog.name.toLowerCase().split(' ').join('_')
  + '.jpg'
 
-          return (<li><p key={UUID()}>{hog.name}</p>
-        <img src={hog_url}/></li>
+          return (<li key={UUID()}><p>{hog.name}</p>
+        <img id={index} src={hog_url} onClick={this.handleImageClick}/></li>
           )
 
         }
       ));
+  }
+
+  handleImageClick = (event) => {
+    console.log("clicked image")
+    this.setState({
+      pigId: event.target.id,
+      page: 'show',
+    }, () => console.log(this.state.pigId));
   }
 
 
